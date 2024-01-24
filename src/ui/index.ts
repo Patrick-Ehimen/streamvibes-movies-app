561f272eaae86eec1cf62a6f7017f562e108c3b8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-24 10:18:53 +0100

    Create index.ts

---
+++ src/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as MobileNavbar } from "./mobile-view-ui/MobileNavbar";
+export { default as MobileFooter } from "./mobile-view-ui/MobileFooter";
