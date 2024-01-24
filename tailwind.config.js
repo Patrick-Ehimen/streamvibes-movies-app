68d4ef10247d3edceb7cc2a12738199f36080142
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-24 10:18:48 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+    screens: {
+      ss: "280px",
+      ssm: "540px",
+      sm: "640px",
+      md: "768px",
+      md1: "912px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
   },
   plugins: [],
-}
-
+};
