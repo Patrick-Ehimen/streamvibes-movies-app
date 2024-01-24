d1a505b5029ed90fbb85019ecaa3df8836b7180b
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-24 01:16:59 +0100

    Create index..ts

---
+++ src/assets/index..ts
@@ -0,0 +1,15 @@
+import SearchBar from "./search.svg";
+import BellNotification from "./bell.svg";
+import DesktopLogo from "./desktop-view/logo.svg";
+import DesktopHeroImg from "./desktop-view/heroImg.svg";
+import MobileHeroImg from "./mobile-view/heroImgMobile.svg";
+import MobileLogo from "./mobile-view/logoMobile.svg";
+
+export {
+  SearchBar,
+  BellNotification,
+  DesktopLogo,
+  MobileHeroImg,
+  MobileLogo,
+  DesktopHeroImg,
+};
