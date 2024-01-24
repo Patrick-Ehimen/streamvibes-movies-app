cd34780c7b674bad89f161697998445410133e75
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-24 10:18:44 +0100

    creating several ui component

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Movies } from "./Movies";
+export { default as Home } from "./Home";
+export { default as Subscriptions } from "./Subscriptions";
+export { default as Support } from "./Support";
