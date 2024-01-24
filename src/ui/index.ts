1955c276d376962186bad43c8203322e66555ec6
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-24 12:45:13 +0100

    Update index.ts

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as MobileNavbar } from "./mobile-view-ui/MobileNavbar";
 export { default as MobileFooter } from "./mobile-view-ui/MobileFooter";
+export { default as Hero } from "./Hero";
