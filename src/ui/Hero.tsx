c38484825b379f91e78f9e420abcf7db7e319c8d
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-29 13:34:21 +0100

    Update Hero.tsx

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   DesktopHeroImg,
   MobileHeroImg,
@@ -12,44 +12,38 @@ const Hero = () => {
   const [desktopLoaded, setDesktopLoaded] = useState(false);
   const [mobileLoaded, setMobileLoaded] = useState(false);
 
-  const handleDesktopImageLoad = () => {
-    setDesktopLoaded(true);
-  };
+  useEffect(() => {
+    // Load desktop image
+    const desktopImage = new window.Image(); // Creating a new image object for the desktop version
+    desktopImage.src = DesktopHeroImg; // Setting the source of the desktop image
+    desktopImage.onload = () => setDesktopLoaded(true); // Setting the desktop image to loaded when it's fully loaded
 
-  const handleMobileImageLoad = () => {
-    setMobileLoaded(true);
-  };
+    // Load mobile image
+    const mobileImage = new window.Image(); // Creating a new image object for the mobile version
+    mobileImage.src = MobileHeroImg; //Setting the source of the image
+    mobileImage.onload = () => setMobileLoaded(true); // Setting the mobile image to loaded when it's fully loaded
+  }, []);
 
   return (
     <section className=" -mt-[110p] z-0">
-      {desktopLoaded ? (
-        <img
-          src={DesktopHeroImg}
-          alt="hero"
-          className="hidden md:block"
-          onLoad={handleDesktopImageLoad}
-        />
+      {desktopLoaded ? ( // Check if desktop image is loaded
+        <img src={DesktopHeroImg} alt="hero" className="hidden md:block" /> // Display desktop image if loaded
       ) : (
-        <img src={LoadDesktopHeroImg} alt="hero" className="hidden md:block" />
+        <img src={LoadDesktopHeroImg} alt="hero" className="hidden md:block" /> // Display placeholder for desktop image if not loaded
       )}
-      <div className="bg-gradient-to-b from-[#141414] h-[30px] hidden md:flex " />
-
-      {mobileLoaded ? (
-        <img
-          src={MobileHeroImg}
-          alt="hero"
-          className="md:hidden flex w-full"
-          onLoad={handleMobileImageLoad}
-        />
+      <div className="bg-gradient-to-b from-[#141414] h-[30px] hidden md:flex " />{" "}
+      {/* Background gradient for desktop */}
+      {mobileLoaded ? ( // Check if mobile image is loaded
+        <img src={MobileHeroImg} alt="hero" className="md:hidden flex w-full" /> // Display mobile image if loaded
       ) : (
         <img
-          src={LoadMobileHeroImg}
+          src={LoadMobileHeroImg} // Display placeholder for mobile image if not loaded
           alt="hero"
           className="md:hidden flex w-full"
         />
       )}
-      <div className="bg-gradient-to-b from-[#141414] h-[30px] md:hidden flex " />
-
+      <div className="bg-gradient-to-b from-[#141414] h-[30px] md:hidden flex " />{" "}
+      {/* Background gradient for mobile */}
       <div className="flex flex-col items-center justify-center font-manrope">
         <div className="md:text-[38px] text-[28px] font-bold text-center mx-[10px] -mt-[40px]">
           The Best Streaming Experience
@@ -66,6 +60,7 @@ const Hero = () => {
           StreamVibe is the best streaming experience for watching your favorite
           movies and shows on demand, anytime, anywhere
         </div>
+        {/* Button to start watching */}
         <Button image={PlayIcon} text="Start Watching Now" />
       </div>
     </section>
