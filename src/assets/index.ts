964d15d383aec075fcf7442eec49817516533baf
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-29 13:34:46 +0100

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,9 @@ import DesktopLogo from "./desktop-view/logo.svg";
 import DesktopHeroImg from "./desktop-view/heroImg.svg";
 import MobileHeroImg from "./mobile-view/heroImgMobile.svg";
 import MobileLogo from "./mobile-view/logoMobile.svg";
+import PlayIcon from "./playIcon.svg";
+import LoadDesktopHeroImg from "./desktop-view/heroImg.png";
+import LoadMobileHeroImg from "./mobile-view/heroImgMobile.png";
 
 export {
   SearchBar,
@@ -12,4 +15,7 @@ export {
   MobileHeroImg,
   MobileLogo,
   DesktopHeroImg,
+  PlayIcon,
+  LoadDesktopHeroImg,
+  LoadMobileHeroImg,
 };
