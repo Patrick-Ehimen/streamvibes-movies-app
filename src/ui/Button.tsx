ffcb5d3b4031d146d1133fdca4b488bb6e79fb09
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-29 13:34:27 +0100

    Update Button.tsx

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,11 +1,19 @@
 interface ButtonProps {
   text: string;
+  image: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text }) => {
+const Button: React.FC<ButtonProps> = ({ text, image }) => {
   return (
-    <button className="bg-[#E50000] text-white rounded-md flex justify-center items-center font-manrope">
-      <div className="p-[14px]">{text}</div>
+    <button className="bg-[#E50000] text-white flex rounded-md my-[30px] font-manrope">
+      {image && (
+        <img
+          src={image}
+          alt=""
+          className="inline-block ml-2 mt-[13px] pr-[2px]"
+        />
+      )}
+      <div className="py-[14px] pr-[18px]">{text}</div>
     </button>
   );
 };
