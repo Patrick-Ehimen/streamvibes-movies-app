fc70c0b9b4cb6d021217b6bb4f5a0129cf1f18ee
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-29 13:11:19 +0100

    Update Hero.tsx

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,27 +1,60 @@
-import { DesktopHeroImg, MobileHeroImg } from "../assets";
+import { useState } from "react";
+import {
+  DesktopHeroImg,
+  MobileHeroImg,
+  PlayIcon,
+  LoadDesktopHeroImg,
+  LoadMobileHeroImg,
+} from "../assets";
 import { Button } from ".";
 
 const Hero = () => {
+  const [desktopLoaded, setDesktopLoaded] = useState(false);
+  const [mobileLoaded, setMobileLoaded] = useState(false);
+
+  const handleDesktopImageLoad = () => {
+    setDesktopLoaded(true);
+  };
+
+  const handleMobileImageLoad = () => {
+    setMobileLoaded(true);
+  };
+
   return (
     <section className=" -mt-[110p] z-0">
-      <img src={DesktopHeroImg} alt="hero" className="hidden md:block" />
-      <div className="bg-gradient-to-b from-[#141414] h-[30px] hidden md:flex " />
-
-      {/* <div className="relative bg-fade-bottom">
+      {desktopLoaded ? (
         <img
           src={DesktopHeroImg}
           alt="hero"
-          className="hidden md:block absolute inset-0"
+          className="hidden md:block"
+          onLoad={handleDesktopImageLoad}
         />
-      </div> */}
-      <img src={MobileHeroImg} alt="hero" className="md:hidden flex w-full" />
+      ) : (
+        <img src={LoadDesktopHeroImg} alt="hero" className="hidden md:block" />
+      )}
+      <div className="bg-gradient-to-b from-[#141414] h-[30px] hidden md:flex " />
+
+      {mobileLoaded ? (
+        <img
+          src={MobileHeroImg}
+          alt="hero"
+          className="md:hidden flex w-full"
+          onLoad={handleMobileImageLoad}
+        />
+      ) : (
+        <img
+          src={LoadMobileHeroImg}
+          alt="hero"
+          className="md:hidden flex w-full"
+        />
+      )}
       <div className="bg-gradient-to-b from-[#141414] h-[30px] md:hidden flex " />
 
-      <div className="flex flex-col items-center justify-center font-manrope ">
+      <div className="flex flex-col items-center justify-center font-manrope">
         <div className="md:text-[38px] text-[28px] font-bold text-center mx-[10px] -mt-[40px]">
           The Best Streaming Experience
         </div>
-        <div className="mx-[200px] text-center hidden md:flex">
+        <div className="lg:mx-[150px] mx-[100px] text-center hidden md:flex mt-[10px]">
           StreamVibe is the best streaming experience for watching your favorite
           movies and shows on demand, anytime, anywhere. With StreamVibe, you
           can enjoy a wide variety of content, including the latest
@@ -33,9 +66,8 @@ const Hero = () => {
           StreamVibe is the best streaming experience for watching your favorite
           movies and shows on demand, anytime, anywhere
         </div>
+        <Button image={PlayIcon} text="Start Watching Now" />
       </div>
-
-      <Button text="Start Watching Now" />
     </section>
   );
 };
