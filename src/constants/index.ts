f0c21f1e18ceb3dff860e304613186253a8ad030
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-30 12:29:34 +0100

    Create index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+import {
+  ActionCarousel,
+  AdventureCarousel,
+  ComedyCarousel,
+  DramaCarousel,
+  HorrorCarousel,
+} from "../assets";
+
+export const homeCarousel = [
+  {
+    id: 1,
+    img: ActionCarousel,
+    title: "Action",
+  },
+  {
+    id: 2,
+    img: AdventureCarousel,
+    title: "Adventure",
+  },
+  {
+    id: 3,
+    img: ComedyCarousel,
+    title: "Comedy",
+  },
+  {
+    id: 4,
+    img: DramaCarousel,
+    title: "Drama",
+  },
+  {
+    id: 5,
+    img: HorrorCarousel,
+    title: "Horror",
+  },
+];
