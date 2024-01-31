ff1cd391a903e943b36375278ac24add145ef956
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-31 22:26:16 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -78,3 +78,62 @@ export const deviceAccess = [
     para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
   },
 ];
+
+export const faq = [
+  {
+    id: 1,
+    num: "1",
+    question: "What is StreamVibe?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 2,
+    num: "2",
+    question: "How much does StreamVibe cost?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 3,
+    num: "3",
+    question: "What content is available on StreamVibe?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 4,
+    num: "4",
+    question: "How can I watch StreamVibe?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 5,
+    num: "5",
+    question: "How do I sign up for StreamVibe?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 6,
+    num: "6",
+    question: "What is the StreamVibe free trial?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 7,
+    num: "7",
+    question: "How do I contact StreamVibe customer support?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+  {
+    id: 8,
+    num: "8",
+    question: "What are the StreamVibe payment methods?",
+    answer:
+      "StreamVibe is a streaming service that allows you to watch movies and shows on demand.",
+  },
+];
