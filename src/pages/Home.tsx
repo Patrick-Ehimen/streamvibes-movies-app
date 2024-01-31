a4819e3beca4f086a8fce9118b161bfdf0441d02
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-31 22:13:23 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
-import { Hero, HomeMovieCategory } from "../ui";
+import { Hero, HomeMovieCategory, DeviceAccess, Faq } from "../ui";
 const Home = () => {
   return (
     <section>
       <Hero />
       <HomeMovieCategory />
+      <DeviceAccess />
+      <Faq />
     </section>
   );
 };
