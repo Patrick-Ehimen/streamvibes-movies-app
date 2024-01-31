09744716a88b544fd6090e026e3bca147f743916
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-31 22:13:44 +0100

    Update MobileCarousel.tsx

--- src/ui/mobile-view-ui/MobileCarousel.tsx
+++ src/ui/mobile-view-ui/MobileCarousel.tsx
@@ -6,23 +6,32 @@ interface CarouselProps {
 
 const MobileCarousel: React.FC<CarouselProps> = ({ items }) => {
   return (
-    <section className="overflow-x-auto mt-[50px] mx-[] font-manrope md:hidden flex ">
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className="mx-[10px] bg-[#262626] p-3 rounded-lg w-[200px] "
-        >
-          <img src={item.img} alt={item.title} className="w-[300px]" />
-          <div className="bg-gradient-to-b from-[#141414] h-[9px] flex" />{" "}
-          <div className="flex">
-            <h2>{item.title}</h2>
-            <div className="flex justify-end my-[5px] ">
-              <FaArrowRight />
+    <div className="">
+      <section className="overflow-x-auto mt-[50px] font-manrope md:hidden flex  ">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="mr-[10px] ml-[20px] bg-[#262626] p-3 rounded-lg w-[200px] px-[20px]   "
+          >
+            <img
+              src={item.img}
+              alt={item.title}
+              className={`w-[500px] ${
+                index === 0 || index === 3 || index === 4 ? "w-[200px]" : ""
+              }`}
+            />
+            <div className="bg-gradient-to-b from-[#141414] h-[9px] flex" />{" "}
+            <div className="flex">
+              <h2>{item.title}</h2>
+              <div className="flex justify-end my-[5px] ">
+                <FaArrowRight />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+      {/* <img src={ActionCarousel} className="w-[] " /> */}
+    </div>
   );
 };
 
