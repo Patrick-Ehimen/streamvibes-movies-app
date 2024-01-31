6dada7dbcda02b00a67fc862591ae40c43ca9e46
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-31 22:13:15 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,12 @@ import {
   ComedyCarousel,
   DramaCarousel,
   HorrorCarousel,
+  Console,
+  Laptop,
+  Phone,
+  Tablet,
+  Tv,
+  Vr,
 } from "../assets";
 
 export const homeCarousel = [
@@ -31,5 +37,44 @@ export const homeCarousel = [
     id: 5,
     img: HorrorCarousel,
     title: "Horror",
+  },
+];
+
+export const deviceAccess = [
+  {
+    id: 1,
+    img: Phone,
+    title: "SmartPhone",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+  },
+  {
+    id: 2,
+    img: Tablet,
+    title: "Tablet",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+  },
+  {
+    id: 3,
+    img: Tv,
+    title: "SmartTv",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+  },
+  {
+    id: 4,
+    img: Laptop,
+    title: "Laptops",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+  },
+  {
+    id: 5,
+    img: Console,
+    title: "GamingConsole",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+  },
+  {
+    id: 6,
+    img: Vr,
+    title: "VR Headsets",
+    para: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
   },
 ];
