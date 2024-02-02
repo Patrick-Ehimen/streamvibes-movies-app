4cab52cb13edc6413b0c45ff9d7e9e73266e5a1f
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-02 12:10:01 +0100

    Update BgImagePhone.tsx

--- src/ui/mobile-view-ui/BgImagePhone.tsx
+++ src/ui/mobile-view-ui/BgImagePhone.tsx
@@ -1,17 +1,32 @@
 import { BgImgPhone } from "../../assets";
+import { Button } from "..";
 
 const BgImagePhone = () => {
   return (
-    <section className="m-[50px] flex md:hidden">
+    <section className="mx-[20px] my-[100px] flex md:hidden">
       <div
         style={{
           backgroundImage: `url(${BgImgPhone})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className=""
+        className="border border-[#262626] rounded-lg"
       >
-        ABCD
+        <div className="my-[100px]">
+          <div className="px-[50px] font-bold text-[25px]">
+            Start your free trial today!
+          </div>
+          <div className="font-regular text-[16px] text-neutral-400 font-normal px-[50px]">
+            This is a clear and concise call to action that encourages users to
+            sign up for a free trial of StreamVibe.
+          </div>
+        </div>
+
+        <Button
+          text="Start Free Trial"
+          image=""
+          className="w-fit h-fit my-[100px] mx-[50px] pl-[10px]"
+        />
       </div>
     </section>
   );
