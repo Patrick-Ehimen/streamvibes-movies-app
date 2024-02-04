d14005c7e1c07a40b80ddc9061e89a804469ea48
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-04 14:16:00 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,8 +1,9 @@
-import { Faq, BgImage, BgImagePhone } from "../ui";
+import { Faq, BgImage, BgImagePhone, ContactForm } from "../ui";
 
 const Support = () => {
   return (
     <section>
+      <ContactForm />
       <Faq />
       <BgImage />
       <BgImagePhone />
