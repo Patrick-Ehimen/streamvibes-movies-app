e77ab26a312d06da122703904c593756c6e7af10
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-06 22:53:21 +0100

    Update Subscriptions.tsx

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,9 +1,10 @@
-import { Pricing, BgImage, BgImagePhone } from "../ui";
+import { Pricing, BgImage, BgImagePhone, Table } from "../ui";
 
 const Subscriptions = () => {
   return (
     <section className="md:mt-[200px] mt-[100px]">
       <Pricing />
+      <Table />
       <BgImage />
       <BgImagePhone />
     </section>
