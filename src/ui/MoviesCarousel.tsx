f06c1bc4cd49440ad2cabf07e20fcb6cd54a2dfd
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-13 21:53:29 +0100

    Update MoviesCarousel.tsx

--- src/ui/MoviesCarousel.tsx
+++ src/ui/MoviesCarousel.tsx
@@ -1,34 +1,40 @@
-// const MoviesCarousel = () => {
-//   return (
-//     <section className="mt-[150px] mx-[20px] md:mx-[50px] ">
-//       MoviesCarousel
-//     </section>
-//   );
-// };
-
-// export default MoviesCarousel;
-
+// import React, { useRef, useState } from 'react';
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css"; // Import Swiper styles
+import { MoviesPageImage } from "../assets";
 
-const MoviesCarousel = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../index.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+export default function App() {
   return (
-    <div className="mt-[150px] mb-[100px] mx-[20px] md:mx-[50px]">
+    <section className="mt-[150px] md:mx-[50px] mx-[20px] mb-[50px] ">
       <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
       >
-        {/* Replace with your movie items */}
-        <SwiperSlide>Movie 1</SwiperSlide>
-        <SwiperSlide>Movie 2</SwiperSlide>
-        <SwiperSlide>Movie 3</SwiperSlide>
-        {/* ... */}
+        <SwiperSlide>{MoviesPageImage} </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>{MoviesPageImage} </SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
-    </div>
+    </section>
   );
-};
-
-export default MoviesCarousel;
+}
