110889174ffb54118fd0f41373944103537d0f9b
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-13 11:26:15 +0100

    Update MobileCarousel.tsx

--- src/ui/mobile-view-ui/MobileCarousel.tsx
+++ src/ui/mobile-view-ui/MobileCarousel.tsx
@@ -1,37 +1,28 @@
-import { FaArrowRight } from "react-icons/fa";
+import { Carousel } from "flowbite-react";
+import { homeCarousel } from "../../constants";
 
-interface CarouselProps {
-  items: Array<{ id: number; img: string; title: string }>;
-}
-
-const MobileCarousel: React.FC<CarouselProps> = ({ items }) => {
+const MobileCarousel = () => {
   return (
-    <div className="">
-      <section className="overflow-x-auto mt-[50px] font-manrope md:hidden flex  ">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="mr-[10px] ml-[20px] bg-[#262626] p-3 rounded-lg w-[200px] px-[20px]   "
-          >
-            <img
-              src={item.img}
-              alt={item.title}
-              className={`w-[500px] ${
-                index === 0 || index === 3 || index === 4 ? "w-[200px]" : ""
-              }`}
-            />
-            <div className="bg-gradient-to-b from-[#141414] h-[9px] flex" />{" "}
-            <div className="flex">
-              <h2>{item.title}</h2>
-              <div className="flex justify-end my-[5px] ">
-                <FaArrowRight />
-              </div>
+    <section className="md:hidden mt-[50px] font-manrope mx-[20px]">
+      <div className="h-[500px]">
+        <Carousel slide={false}>
+          {/* <div className=""> */}
+          {homeCarousel.map((item) => (
+            <div key={item.id} className="bg-[#262626] p-[20px] rounded-b-xl">
+              <img
+                src={item.img}
+                alt={item.title}
+                className="w-96 pt-[15px] rounded-lg"
+              />
+              <p className="text-white text-base py-4 font-bold">
+                {item.title}
+              </p>
             </div>
-          </div>
-        ))}
-      </section>
-      {/* <img src={ActionCarousel} className="w-[] " /> */}
-    </div>
+          ))}
+          {/* </div> */}
+        </Carousel>
+      </div>
+    </section>
   );
 };
 
