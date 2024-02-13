5b3ea91a14229c4dc07938ac34b1dc09429edc56
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-02-13 12:59:37 +0100

    Update HomeMovieCategory.tsx

--- src/ui/home-ui/HomeMovieCategory.tsx
+++ src/ui/home-ui/HomeMovieCategory.tsx
@@ -1,5 +1,7 @@
 import { HomeCarousel } from "../index";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
+// Define the HomeMovieCategory functional component
 const HomeMovieCategory = () => {
   return (
     <section>
@@ -14,11 +16,34 @@ const HomeMovieCategory = () => {
           </p>
         </div>
 
-        <div className="mx-[100px] hidden md:flex">ABC</div>
+        <div className="mx-[50px] flex h-14 hidden md:flex border border-[#262626] rounded-lg bg-[#0F0F0F]">
+          {/* Left arrow for navigation */}
+          <div className="bg-[#262626] p-[10px] m-[10px] rounded-lg cursor-pointer">
+            <FaArrowLeft size={12} />
+          </div>
+          {/* Display pagination indicators */}
+          <div className="flex flex-row">
+            {[1, 2, 3, 4].map((item, index) => (
+              <div
+                key={item}
+                className={`my-[12px] text-[20px] font-bold px-[2px] ${
+                  index === 0 ? "text-[#e50000]" : ""
+                } `}
+              >
+                -
+              </div>
+            ))}
+          </div>
+          {/* Right arrow for navigation */}
+          <div className="bg-[#262626] p-[10px] m-[10px] rounded-lg cursor-pointer">
+            <FaArrowRight size={12} />
+          </div>
+        </div>
       </div>
       <HomeCarousel />
     </section>
   );
 };
 
+// The HomeMovieCategory component is exported
 export default HomeMovieCategory;
